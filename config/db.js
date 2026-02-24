709f617259377ef1b4242e2e28224f116a5645c8
devanshumankar <devanshumankar@gmail.com>
2026-02-24 18:24:52 +0530

    database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose"
+import dns from "dns"
+
+// Force Google DNS to resolve MongoDB Atlas hostnames (system DNS blocks *.mongodb.net)
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
     try {
